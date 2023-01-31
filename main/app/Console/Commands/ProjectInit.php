4b62b93451a0ea2b1f4e14667b674ff1cd52bace
Mostafa Nabawy <ms.amin@gmail.com>
2023-01-31 12:23:46 +0300

    main microservice setup

---
+++ main/app/Console/Commands/ProjectInit.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+
+class ProjectInit extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'project:init';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Init the project';
+
+    /**
+     * Execute the console command.
+     *
+     * @return int
+     */
+    public function handle()
+    {
+        $this->call('key:generate');
+        $this->call('migrate');
+        // $this->call('db:seed');
+    }
+}
---
+++ main/app/Http/Controllers/ProductController.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Product;
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+use App\Http\Resources\ProjectResource;
+
+class ProductController extends Controller
+{
+    public function index()
+    {
+        return Product::all();
+    }
+
+    public function show($id)
+    {
+        return Product::find($id);
+    }
+
+    public function store(Request $request)
+    {
+        $product = Product::create($request->only('title', 'image'));
+
+        // ProductCreated::dispatch($product->toArray())->onQueue('main_queue');
+
+        return response($product, Response::HTTP_CREATED);
+    }
+
+    public function update($id, Request $request)
+    {
+        $product = Product::find($id);
+
+        $product->update($request->only('title', 'image'));
+
+        // ProductUpdated::dispatch($product->toArray())->onQueue('main_queue');
+
+        return response($product, Response::HTTP_ACCEPTED);
+    }
+
+    public function destroy($id)
+    {
+        Product::destroy($id);
+
+        // ProductDeleted::dispatch($id)->onQueue('main_queue');
+
+        return response(null, Response::HTTP_NO_CONTENT);
+    }
+}
